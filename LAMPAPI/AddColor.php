6a3587fa20ce2370017fcfc9885b338fccc40794
Kaden Nash <kadenrnash05@gmail.com>
2026-04-23 14:47:19 -0400

    Testing CI/CD pipeline.

--- LAMPAPI/AddColor.php
+++ LAMPAPI/AddColor.php
@@ -1,49 +1,47 @@
 <?php
-	$inData = getRequestInfo();
-	
-	$color = $inData["color"];
-	$userId = $inData["userId"];
+$inData = getRequestInfo();
 
-		// Load the .env file
-	$env = parse_ini_file(__DIR__ . '/.env');
+$color = $inData["color"];
+$userId = $inData["userId"];
 
-	// Use the variables
-	$conn = new mysqli(
-		$env["DB_SERVER"], 
-		$env["DB_USER"], 
-		$env["DB_PASS"], 
-		$env["DB_NAME"]
-	);
-	
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
-		$stmt->bind_param("ss", $userId, $color);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+// Load the .env file
+$env = parse_ini_file(__DIR__ . '/.env');
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+// Use the variables
+$conn = new mysqli(
+	$env["DB_SERVER"],
+	$env["DB_USER"],
+	$env["DB_PASS"],
+	$env["DB_NAME"]
+);
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+
+if ($conn->connect_error) {
+	returnWithError($conn->connect_error);
+} else {
+	$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
+	$stmt->bind_param("ss", $userId, $color);
+	$stmt->execute();
+	$stmt->close();
+	$conn->close();
+	returnWithError("");
+}
+
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+function returnWithError($err)
+{
+	$retValue = '{"error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
+}
+
 ?>
